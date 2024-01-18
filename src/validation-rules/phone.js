15479cea9bbaafecc28d504cd4f340cfb23f52e0
Chris Warnes <Chris.Warnes@crafted.co.uk>
2024-01-18 16:31:26 +0000

    allow + characters at the beginning of phone numbers

--- src/validation-rules/phone.js
+++ src/validation-rules/phone.js
@@ -3,7 +3,7 @@ export default function () {
   addRule('valPhone', function (field) {
     return {
       type: 'string',
-      pattern: /^[0-9\s-()]+$/,
+      pattern: /^\+?[0-9\s-()]+$/,
       message: field.dataset.valPhone
     }
   })
